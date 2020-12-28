97d5d2de7e5b917b98b3c28b3665463f42d985da
baojun_a <baojun_a@aspirecn.com>
2020-12-28 16:38:17 +0800

    新增kafka测试类

---
+++ src/test/java/com/chelsea/covid/KafkaTest.java
@@ -0,0 +1,31 @@
+package com.chelsea.covid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * kafka测试类
+ * 
+ * @author shevchenko
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaTest {
+    
+    @Autowired
+    private KafkaTemplate<?, String> kafkaTemplate;
+    
+    /**
+     * 往kafka发送消息测试
+     */
+    @Test
+    public void sendToKafkaTest() {
+        kafkaTemplate.send("test", "abc");
+    }
+
+}

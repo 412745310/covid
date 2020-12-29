44ac2b6524c9c605755e45a6b26be531fe509b7d
baojun_a <baojun_a@aspirecn.com>
2020-12-29 16:40:41 +0800

    新增定时任务功能

--- src/main/java/com/chelsea/covid/App.java
+++ src/main/java/com/chelsea/covid/App.java
@@ -8,9 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class App {
 
     public static void main(String[] args) {

142ba71a74afb393d5afc6e8db09436f286af25b
baojun_a <baojun_a@aspirecn.com>
2020-12-30 14:47:35 +0800

    新增疫情物资生成定时job

---
+++ src/main/java/com/chelsea/covid/job/MateriaGeneratorJob.java
@@ -0,0 +1,45 @@
+package com.chelsea.covid.job;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+import com.chelsea.covid.bean.Materia;
+
+/**
+ * 疫情物资生成定时job
+ * 
+ * @author shevchenko
+ *
+ */
+@Component
+public class MateriaGeneratorJob {
+
+    private String[] wzmc = new String[] {"N95口罩/个", "医用外科口罩/个", "84消毒液/瓶", "电子体温计/个", "一次性手套/副", "护目镜/副", "医用防护服/套"};
+    private String[] wzly = new String[] {"采购", "下拨", "捐赠", "消耗", "需求"};
+    
+    @Autowired
+    private KafkaTemplate<?, String> kafkaTemplate;
+    
+    /**
+     * 疫情物资随机生成
+     */
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void execute() {
+        generator();
+    }
+
+    public void generator() {
+        Random random = new Random();
+        for(int i = 0; i < 10; i++) {
+            Materia materia = new Materia(wzmc[random.nextInt(wzmc.length)], wzly[random.nextInt(wzly.length)], random.nextInt(1000));
+            String jsonString = JSON.toJSONString(materia);
+            kafkaTemplate.send("covid19_wz", jsonString);
+        }
+    }
+
+}

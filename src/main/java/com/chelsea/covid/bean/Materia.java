b9bee6f67394096ca74449de8ebb9c3f1d16ff19
baojun_a <baojun_a@aspirecn.com>
2020-12-30 14:47:10 +0800

    新增实体bean

---
+++ src/main/java/com/chelsea/covid/bean/Materia.java
@@ -0,0 +1,25 @@
+package com.chelsea.covid.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 防疫物资实体类
+ * 
+ * @author shevchenko
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Materia {
+    
+    // 物资名称
+    private String name;
+    // 物资来源
+    private String from;
+    // 物资数量
+    private Integer count;
+
+}

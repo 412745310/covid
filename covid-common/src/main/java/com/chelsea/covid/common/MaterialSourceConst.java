32d792f8ab045397af1372d36572ecdea79f4c6b
baojun_a <baojun_a@aspirecn.com>
2021-01-05 11:22:50 +0800

    新增common模块

---
+++ covid-common/src/main/java/com/chelsea/covid/common/MaterialSourceConst.java
@@ -0,0 +1,21 @@
+package com.chelsea.covid.common;
+
+/**
+ * 物资来源常量
+ * 
+ * @author shevchenko
+ *
+ */
+public interface MaterialSourceConst {
+    
+    String CAIGOU = "采购";
+    
+    String XIABO = "下拨";
+    
+    String JUANZENG = "捐赠";
+    
+    String XIAOHAO = "消耗";
+    
+    String XUQIU = "需求";
+
+}

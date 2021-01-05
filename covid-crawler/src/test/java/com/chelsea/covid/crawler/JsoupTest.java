f1a642958b828c406c4712889785bd7807ed3109
baojun_a <baojun_a@aspirecn.com>
2021-01-05 11:26:17 +0800

    修改实体类位置

--- covid-crawler/src/test/java/com/chelsea/covid/crawler/JsoupTest.java
+++ covid-crawler/src/test/java/com/chelsea/covid/crawler/JsoupTest.java
@@ -18,9 +18,9 @@ import org.springframework.web.client.RestTemplate;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.chelsea.covid.crawler.bean.Covid;
 import com.chelsea.covid.crawler.util.DateUtil;
 import com.chelsea.covid.crawler.util.HttpUtils;
+import com.chelsea.covid.domain.Covid;
 
 /**
  * 爬虫单元测试

4cd627848facc4497f34d2bd7037f1810c82f02a
baojun_a <baojun_a@aspirecn.com>
2021-01-08 11:36:04 +0800

    新增疫情物资DAO

---
+++ covid-process/src/main/java/com/chelsea/covid/process/dao/CovidWzDao.java
@@ -0,0 +1,14 @@
+package com.chelsea.covid.process.dao;
+
+import com.chelsea.covid.domain.CovidWz;
+
+public interface CovidWzDao {
+    
+    /**
+     * 新增物资
+     * 
+     * @param covidWz
+     */
+    void addCovidWz(CovidWz covidWz);
+
+}
